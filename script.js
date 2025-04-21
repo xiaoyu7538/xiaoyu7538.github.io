31a4089b86f5225b00c7f251298176047d579e08
xiaoyu7538 <shaoaoye_1@qq.com>
2025-04-22 00:13:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -52,9 +52,10 @@ noButton.addEventListener("click", function () {
   // 图片变化（前 5 次变化）
   if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
   if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
-  if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
-  if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
-  if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
+   if (clickCount === 3) mainImage.src = "images/think.png";https://github.com/xiaoyu7538/xiaoyu7538.github.io/blob/main/images/1_3d4138d26dd2551ae35de75fb663f8c6.jpg
+  if (clickCount === 4) mainImage.src = "images/angry.png"; // 生气
+  if (clickCount === 5) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount >= 6) mainImage.src = "images/crying.png"; // 之后一直是哭
 });
 
 // Yes 按钮点击后，进入表白成功页面
